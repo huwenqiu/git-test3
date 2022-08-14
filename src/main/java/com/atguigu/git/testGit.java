b486fcb5ee039cfe46a3107031ea8d5eb2336af9
huwenqiu <85096541+huwenqiu@users.noreply.github.com>
2022-08-14 20:34:25 +0800

    Update testGit.java

--- src/main/java/com/atguigu/git/testGit.java
+++ src/main/java/com/atguigu/git/testGit.java
@@ -10,5 +10,6 @@ public class testGit {
         System.out.println("master的测试merge");
         System.out.println("gitee的测试pull");
         System.out.println("在github上写点代码pull到本地库");
+        System.out.println("随便写点东西，测试一下子github和gitee的关联")
     }
 }
